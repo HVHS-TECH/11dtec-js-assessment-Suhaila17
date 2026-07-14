edfbfad45ddd2a2d56568bd5f0320e2bb90b6e8c
Suhaila17 <24014sa@hvhs.school.nz>
2026-07-14 08:50:16 +0000

    Jul 14, 2026, 8:50 AM

--- MenuScript.js
+++ MenuScript.js
@@ -1,10 +1,13 @@
 console.log("Running MenuScript.js")
 
-const menuItems = {
-    redVelvet:{name: "Red Velvet Cake", price: 20},
-    strawberryCheesecake:{name:"Strawberry Ganache Cheesecake", price:22},
-    cookiesCream:{name:"Cookies and Cream Red velvet Cake", price:23},
-    dessertCup:{name:"Dessert Cup", price:10},
-    shortcake:{name:"Strawberry Shortcake", price:20},
-    whiteVelvet:{name:"White Velvet Bliss Cake"}
-};
+const menuItems = [
+  {name: "Red Velvet Cake", price: 20},
+  {name:"Strawberry Ganache Cheesecake", price:22},
+  {name:"Cookies and Cream Red velvet Cake", price:23},
+  {name:"Dessert Cup", price:10},
+  {name:"Strawberry Shortcake", price:20},
+  {name:"White Velvet Bliss Cake"}
+];
+
+let order = [];
+let totalPrice
